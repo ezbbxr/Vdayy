d34bd9e357c088bc840db0d3b64f3e018540a9d8
ezbbxr <159946102+ezbbxr@users.noreply.github.com>
2024-02-14 09:29:17 +0500

    Update vday.js

--- vday.js
+++ vday.js
@@ -1,8 +1,9 @@
-function showAnimation() {
-    document.getElementById("valentineForm").style.display = "none";
-    document.getElementById("animationContainer").style.display = "block";
-}
-
-function showNoResponse() {
-    document.getElementById("noResponse").style.display = "block";
+function submitAnswer() {
+    var valentineAnswer = document.querySelector('input[name="valentineAnswer"]:checked');
+    if (valentineAnswer && valentineAnswer.value === "yes") {
+        document.getElementById("valentineForm").style.display = "none";
+        document.getElementById("animationContainer").style.display = "block";
+    } else {
+        document.getElementById("noResponse").style.display = "block";
+    }
 }
