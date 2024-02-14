9ad94768ed33f6773ad8b44aca8c61b138d422fd
ezbbxr <159946102+ezbbxr@users.noreply.github.com>
2024-02-14 08:54:26 +0500

    Add files via upload

---
+++ vday.js
@@ -0,0 +1,8 @@
+function showAnimation() {
+    document.getElementById("valentineForm").style.display = "none";
+    document.getElementById("animationContainer").style.display = "block";
+}
+
+function showNoResponse() {
+    document.getElementById("noResponse").style.display = "block";
+}
